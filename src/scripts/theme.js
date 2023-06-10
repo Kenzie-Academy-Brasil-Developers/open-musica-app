a7be40939610ff95334140b052caf733139cb764
HalianRC <125613474+HalianRC@users.noreply.github.com>
2023-06-09 22:40:21 -0300

    open-music

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,26 @@
-/* Desenvolva sua lógica aqui ... */
+
+
+function darkMood(){
+  const buttonDarkMood = document.querySelector(".header-button-darkmood")
+  const body = document.querySelector(".darkmood")
+
+  buttonDarkMood.addEventListener("click",() => {
+    body.classList.toggle("darkmood") 
+  })
+}
+darkMood()
+
+
+
+function changeImageDark() {
+  const buttonDarkMood = document.querySelector(".header-button-darkmood");
+  const image = document.querySelector(".image-darkmood");
+
+  buttonDarkMood.addEventListener("click", () => {
+    image.classList.toggle("image-darkmood-moon")
+    image.classList.toggle("image-darkmood-sun")
+  });
+}
+
+changeImageDark();
+
